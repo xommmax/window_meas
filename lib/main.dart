4661035b9bb8f9eae846a1eaa79385afc4aa8820
Max <max.khobotin@futureplatforms.com>
2024-06-24 14:27:55 +0300

    Connect Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -3,14 +3,20 @@ import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:window_meas/common/service_locator.dart';
 import 'package:window_meas/common/view/colors.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 import 'app.dart';
 import 'common/bloc_observer.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await configureDependencies();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   Bloc.observer = const AppBlocObserver();
 
